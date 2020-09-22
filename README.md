98e4aea503d8238ece627b0f746495804ac01219
jpfeiffer16 <joepfeiffer16@gmail.com>
2020-09-21 19:40:31 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# angeldoc-vscode
+
+![Logo](assets/AngelDocLogo.png)
+
+AngelDoc is an open-source replacement for GhostDoc.
+
+## Features
+
+AngelDoc inserts pre-filled xml doc snippets for C# symbols.
+Currently supported are:
+
+* Classes
+* Interfaces
+* Constructors
+* Methods
+* Fields
+* Properties
+
+## Requirements
+
+vscode ^1.46.0
+
+<!-- ## Extension Settings
+
+Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
+
+For example:
+
+This extension contributes the following settings:
+
+* `myExtension.enable`: enable/disable this extension
+* `myExtension.thing`: set to `blah` to do something -->
+
+## Known Issues
+
+AngelDoc does not integrate with Omnisharp-vim and uses a stand-alone .net binary to get info about the symbol you are documenting. This is so that it can work accross multiple editors. For the time being it will be stuck with this and that will limit future possiblities, but for now it achieves it's goals well enough.
---
+++ src/extension.ts
@@ -0,0 +1,100 @@
+import * as vscode from 'vscode';
+import * as childProc from 'child_process';
+import * as path from 'path';
+import * as fs from 'fs';
+import fetch from 'node-fetch';
+import mkdirp = require('mkdirp');
+import extract = require('extract-zip');
+
+const angelDocBinPath = path.join(
+	(process.env.HOME ? process.env.HOME : process.env.USERPROFILE) ?? '~',
+	'/.angeldoc/bin');
+const angelDocDllPath = path.join(angelDocBinPath, '/AngelDoc.dll');
+
+export async function activate(context: vscode.ExtensionContext) {
+	let disposable = vscode.commands.registerCommand('angeldoc-vscode.installangeldoc', async () => {
+		await installAngelDoc();
+	});
+	context.subscriptions.push(disposable);
+	await ensureInstalled();
+	console.log('installed');
+	let repeatedSlashes = 0;
+	vscode.workspace.onDidChangeTextDocument(e => {
+		if (e.contentChanges) {
+			if (e.contentChanges[0].text === '/') {
+				repeatedSlashes++;
+			}
+			else {
+				repeatedSlashes = 0;
+			}
+			if (repeatedSlashes === 3) {
+				if (vscode.window.activeTextEditor) {
+					const editor = vscode.window.activeTextEditor;
+
+					vscode.window.activeTextEditor?.edit(editBuilder =>
+						editBuilder.delete(
+							new vscode.Range(editor.selection.active.line, 0, editor.selection.active.line + 1, 0)));
+
+					const proc = childProc.spawn(
+						'dotnet',
+						[
+							angelDocDllPath,
+							'-',
+							'gendoccsharp',
+							(editor.selection.active.line + 1).toString()
+						], {
+						stdio: 'pipe'
+					}
+					);
+
+					let output = '';
+
+					proc.stdout.on('data', data => {
+						output += data.toString();
+					});
+					proc.stdout.on('end', () => {
+						editor.edit(editBuilder =>
+							editBuilder.insert(new vscode.Position(editor.selection.active.line, 0), output + '\n'));
+						vscode.commands.executeCommand('editor.action.formatDocument');
+					});
+					proc.stderr.on('data', data => {
+						console.error(data.toString());
+					});
+					proc.stdin.setDefaultEncoding('utf-8');
+					proc.stdin.write(editor.document.getText(), (err) => {
+						if (err) {
+							console.error(err);
+							return;
+						}
+						proc.stdin.end();
+					});
+				}
+			}
+		}
+	});
+}
+
+export function deactivate() {
+	console.log('AngelDoc Deactivated.');
+}
+
+async function ensureInstalled() {
+	if (!fs.existsSync(angelDocDllPath)) {
+		await installAngelDoc();
+	}
+}
+
+function installAngelDoc(): Promise<void> {
+	const promise = new Promise<void>(async (resolve, reject) => {
+		mkdirp(angelDocBinPath);
+		const releasesResponse = await (await fetch('https://api.github.com/repos/jpfeiffer16/AngelDoc/releases/latest')).json();
+		const latestRelease = releasesResponse.tag_name;
+		const res = (await fetch(`https://github.com/jpfeiffer16/AngelDoc/releases/download/${latestRelease}/AngelDoc.zip`));
+		res.body.pipe(fs.createWriteStream(path.join(angelDocBinPath, 'AngelDoc.zip')));
+		res.body.on('end', async () => {
+			await extract(path.join(angelDocBinPath, '/AngelDoc.zip'), { dir: angelDocBinPath });
+			resolve();
+		});
+	});
+	return promise;
+}
